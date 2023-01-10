5e62149ccd77cbf93631fcb8dc3e2af9455bf9de
ChoiSuJin03 <106504517+ChoiSuJin03@users.noreply.github.com>
2023-01-10 20:24:02 +0900

    Initial commit

---
+++ src/main/java/one/onespring/OneSpringApplication.java
@@ -0,0 +1,13 @@
+package one.onespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneSpringApplication.class, args);
+	}
+
+}
---
+++ src/main/java/one/onespring/controller/OneController.java
@@ -0,0 +1,49 @@
+package one.onespring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class OneController {
+    @GetMapping("one")
+    public String one(Model model) {
+        model.addAttribute("data", "Spring!!" );
+        return "one"; // <- 여기로 가서 실행 시켜라
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
